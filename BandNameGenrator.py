f636779fce21caa49b88b6090a51846eb0b904cb
Jake Merlin <JacobMerlin92@outlook.com>
2022-05-28 12:36:17 -0400

    This is a program to combin basic varibles and print / input fuctions to create a fun little app showcasing sytax knowlegde

---
+++ BandNameGenrator.py
@@ -0,0 +1,11 @@
+#This is a program to combin basic varibles and print / input fuctions to create a fun little app showcasing sytax knowlegde
+#1. Greeting for my program
+print("Welcome to the Band Name Genrator Program Or BNGP for shorthand Please follow all               instructions as listed for best intended results.")
+#2. Ask user what city they grew up in
+city = input("What city did you grow up in?")
+#3.ask user for their pet name
+firstPet = input("What is the name of your first pet?")
+#4 combin the name of their city and pet to give a band name
+length = (city + firstPet)
+#then print value of the total sum for the data incoming data
+print(length)
