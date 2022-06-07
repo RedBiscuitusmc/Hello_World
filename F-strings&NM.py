321cfe6e79b281b0b7a8920439688062ac149289
Jake Merlin <JacobMerlin92@outlook.com>
2022-06-07 09:00:28 -0400

    F-strings and Number manipulation showcase

---
+++ F-strings&NM.py
@@ -0,0 +1,10 @@
+#Number Manipulation and F-Strings
+#Varible 1
+score = 0
+#Varible 2
+height = 1.8
+#Varible 3
+isWinning = True
+#F-string
+# \n break added to print information more cleanly
+print(f"your score is {score},\n your height is {height},\n you are winning is {isWinning}")
