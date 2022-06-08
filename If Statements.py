1d7f13862e4f27a5642912b91011425830b34f45
Jake Merlin <JacobMerlin92@outlook.com>
2022-06-08 11:54:59 -0400

    conditional statements Logical Operators

---
+++ If Statements.py
@@ -0,0 +1,13 @@
+#Coding BootCamp Challenge Udemy
+#Section 3
+#Control Oprations
+#Jacob Merlin
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm?\n "))
+
+if height >= 120 :
+    print("You can ride this rollercoaster!")
+else :
+    print("Sorry, you have to grow taller before you can ride.")
+
