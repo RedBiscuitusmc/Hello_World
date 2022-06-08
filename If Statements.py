4486147956b4c9ea3142e9a25babe2e478ec888c
Jake Merlin <JacobMerlin92@outlook.com>
2022-06-08 13:09:14 -0400

    odd or even code challenge using IF statements.

--- If Statements.py
+++ If Statements.py
@@ -3,11 +3,19 @@
 #Control Oprations
 #Jacob Merlin
 
-print("Welcome to the rollercoaster!")
-height = int(input("What is your height in cm?\n "))
+print("Welcome to the rollercaoster!")
+#Input Request
+height = int (input("What is your height in cm?\n"))
 
-if height >= 120 :
-    print("You can ride this rollercoaster!")
-else :
+#IF Statement
+if height >= 120:
+    print("You can ride the rollercoaster")
+    age = int (input("What is your age?\n"))
+    if age <= 12:
+        print("Please pay $5")
+    elif age <= 18:
+        print("Your Ticket costs $7")
+    else:
+        print("Please pay $12")
+else:
     print("Sorry, you have to grow taller before you can ride.")
-
